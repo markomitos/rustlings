1b1246307b8f4fe6c74481c3c0605e206ba158d1
Marko.Mitosevic <marko.mitosevic@jetbrains.com>
2025-02-26 15:47:23 +0100

    finished options exercises

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -28,6 +28,23 @@ mod my_module {
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output = vec![];
+        for (string, command) in input {
+            match command {
+                Command::Uppercase => output.push(string.to_uppercase()),
+                Command::Trim => output.push(string.trim().to_string()),
+                Command::Append(n) => {
+                    let mut new_string = string.clone();
+                    for _ in 0..n {
+                        new_string.push_str("bar");
+                    }
+                    output.push(new_string.clone());
+                }
+            }
+        }
+        output
+    }
 }
 
 fn main() {
@@ -38,6 +55,7 @@ fn main() {
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
